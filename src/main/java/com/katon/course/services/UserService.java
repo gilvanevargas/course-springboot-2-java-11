00b781b31b0c137cee783afac9c016daa5dca46d
Gilvane Vargas <gilvane.vargas@gmail.com>
2021-03-12 16:58:51 -0300

    Service layer, component registration

---
+++ src/main/java/com/katon/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.katon.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.katon.course.entities.User;
+import com.katon.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
