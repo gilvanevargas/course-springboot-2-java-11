c80199757e1c0368e6e5285bc1921d105eb1ecb8
Gilvane Vargas <gilvane.vargas@gmail.com>
2021-03-15 15:37:39 -0300

    Payment, one-to-one association

--- src/main/java/com/katon/course/config/TestConfig.java
+++ src/main/java/com/katon/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.katon.course.entities.Category;
 import com.katon.course.entities.Order;
 import com.katon.course.entities.OrderItem;
+import com.katon.course.entities.Payment;
 import com.katon.course.entities.Product;
 import com.katon.course.entities.User;
 import com.katon.course.entities.enums.OrderStatus;
@@ -81,6 +82,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
