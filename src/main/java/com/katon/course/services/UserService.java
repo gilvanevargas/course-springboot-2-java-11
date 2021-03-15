b86a63fceea6334dbf9e5f25ffdaa38597ae2931
Gilvane Vargas <gilvane.vargas@gmail.com>
2021-03-15 18:06:52 -0300

    Exception handling - findById

--- src/main/java/com/katon/course/services/UserService.java
+++ src/main/java/com/katon/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.katon.course.entities.User;
 import com.katon.course.repositories.UserRepository;
+import com.katon.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
